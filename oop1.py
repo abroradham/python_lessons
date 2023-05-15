83b6dd791176228c6f1bcd8727d417761cc265ce
Abror Adham <126870169+abroradham@users.noreply.github.com>
2023-05-15 13:00:08 +0500

    one new file added

---
+++ oop1.py
@@ -0,0 +1,33 @@
+class User:
+    def __init__(self, username, ism, familiya, email, telefon_raqam):
+        
+        self.username = username
+        self.name = ism
+        self.surname = familiya
+        self.email = email
+        self.phone_number = telefon_raqam
+
+    def get_name(self):
+        return self.name
+    
+    def get_username(self):
+        return self.username
+    
+    def get_surname(self):
+        return self.surname
+    
+    def get_emai(self):
+        return self.email
+    
+    def get_phone_number(self):
+        return self.phone_number
+    
+    def get_info(self):
+        return f"""Foydalanuvchi: {self.username} \n Ismi: {self.name.title()} {self.surname.title()},  Email: {self.email}, Telefon raqami: {self.phone_number}"""
+
+
+user1 = User("abroradham", "Abror", "Nematjanov", "abrornematjanov1@gmail.com", +998913450551)
+
+
+
+print(user1.get_info())
